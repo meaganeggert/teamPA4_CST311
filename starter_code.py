858984469fe8b2a582aa163d76704d1f990e6393
HoppensTech <bhoppens@csumb.edu>
2024-10-15 15:36:43 -0700

    42/42 packets received

--- starter_code.py
+++ starter_code.py
@@ -43,29 +43,32 @@ def myNetwork():
     net.addLink(h2, s1)
     net.addLink(h3, s2)
     net.addLink(h4, s2)
-    # net.addLink(s2, r5,intfName2='r5-eth2',params2={'ip':'10.0.2.0/24'})
-    # net.addLink(s1, r3,intfName2='r3-eth2',params2={'ip':'10.0.1.0/24'})
     net.addLink(s2, r5)
     net.addLink(s1, r3)
     net.addLink(r3, r4,intfName1='r3-eth1',params1={'ip':'192.168.1.2/30'}, intfName2='r4-eth0', params2={'ip':'192.168.1.1/30'})
-    # net.addLink(r4, r5,intfName1='r4-eth1',params1={'ip':'192.168.2.1/30'}, intfName2='r5-eth1', params2={'ip':'192.168.2.2/30'})
-    net.addLink(r5, r4,intfName1='r5-eth1',params1={'ip':'192.168.2.2/30'}, intfName2='r4-eth1', params2={'ip':'192.168.2.1/30'})
+    net.addLink(r4, r5,intfName1='r4-eth1',params1={'ip':'192.168.2.1/30'}, intfName2='r5-eth1', params2={'ip':'192.168.2.2/30'})
+
     info( '*** Starting network\n')
     net.build()
-    # ___________________________________________________So far allows 27/42 connections 
-    net['r3'].cmd('ip route add 192.168.1.0/30 via 192.168.1.1 dev r3-eth1')
-    net['r3'].cmd('ip route add 192.168.2.0/30 via 192.168.1.1 dev r3-eth1')
-    net['r3'].cmd('ip route add 10.0.2.0/24 via 192.168.1.1 dev r3-eth1')
+    # ___________________________________________________hopefully correct way to route everything
+    # allow hosts to send packets to its outside networks
+    net['h1'].cmd('ip route add 10.0.2.0/24 via 10.0.1.0')
+    net['h2'].cmd('ip route add 10.0.2.0/24 via 10.0.1.0')
+    net['h3'].cmd('ip route add 10.0.1.0/24 via 10.0.2.0')
+    net['h4'].cmd('ip route add 10.0.1.0/24 via 10.0.2.0')
 
+    # Forward anything related to outside networks via r4-eth1 from r5
+    net['r5'].cmd('ip route add 10.0.1.0/24 via 192.168.2.1 dev r5-eth1')
+    net['r5'].cmd('ip route add 192.168.1.0/30 via 192.168.2.1 dev r5-eth1')
+
+    # Forward anything related to r3 network via r3-eth1 from r4
+    # Forward anything related to r5 network via r5-eth1 from r4
     net['r4'].cmd('ip route add 10.0.1.0/24 via 192.168.1.2 dev r4-eth0')
     net['r4'].cmd('ip route add 10.0.2.0/24 via 192.168.2.2 dev r4-eth1')
-    net['r4'].cmd('ip route add 192.168.1.0/30 via 192.168.1.2 dev r4-eth0')
-    net['r4'].cmd('ip route add 192.168.2.0/30 via 192.168.2.2 dev r4-eth1')
-    
-    net['r5'].cmd('ip route add 192.168.1.0/30 via 192.168.2.1 dev r5-eth1')
-    net['r5'].cmd('ip route add 192.168.2.0/30 via 192.168.2.1 dev r5-eth1')
-    net['r5'].cmd('ip route add 10.0.1.0/24 via 192.168.2.1 dev r5-eth1')
-    
+
+    # Forward anything related to outside networks via r4-eth0 from r3
+    net['r3'].cmd('ip route add 10.0.2.0/24 via 192.168.1.1 dev r3-eth1') 
+    net['r3'].cmd('ip route add 192.168.2.0/30 via 192.168.1.1 dev r3-eth1')
     # ___________________________________________________
     info( '*** Starting controllers\n')
     for controller in net.controllers:
