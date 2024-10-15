615c5e492b0748f76494765d4a9a01092eb2a7db
HoppensTech <bhoppens@csumb.edu>
2024-10-15 12:47:21 -0700

    27/42 packets recieved

--- starter_code.py
+++ starter_code.py
@@ -23,33 +23,44 @@ def myNetwork():
                       port=6633)
 
     info( '*** Add switches\n')
-    r5 = net.addHost('r5', cls=Node, ip='0.0.0.0')
-    r5.cmd('sysctl -w net.ipv4.ip_forward=1')
-    s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
+    s2 = net.addSwitch('s2', cls=OVSKernelSwitch)   #added switches before routers
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
-    r4 = net.addHost('r4', cls=Node, ip='0.0.0.0')
+    r5 = net.addHost('r5', cls=Node, ip='10.0.2.1/24')
+    r5.cmd('sysctl -w net.ipv4.ip_forward=1')
+    r4 = net.addHost('r4', cls=Node, ip='192.168.1.2/22')
     r4.cmd('sysctl -w net.ipv4.ip_forward=1')
-    r3 = net.addHost('r3', cls=Node, ip='0.0.0.0')
+    r3 = net.addHost('r3', cls=Node, ip='10.0.1.1/24')
     r3.cmd('sysctl -w net.ipv4.ip_forward=1')
 
     info( '*** Add hosts\n')
-    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
-    h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
-    h3 = net.addHost('h3', cls=Host, ip='10.0.0.3', defaultRoute=None)
-    h4 = net.addHost('h4', cls=Host, ip='10.0.0.4', defaultRoute=None)
+    h1 = net.addHost('h1', cls=Host, ip='10.0.1.2', defaultRoute='via 10.0.1.1')
+    h2 = net.addHost('h2', cls=Host, ip='10.0.1.3', defaultRoute='via 10.0.1.1')
+    h3 = net.addHost('h3', cls=Host, ip='10.0.2.2', defaultRoute='via 10.0.2.1')
+    h4 = net.addHost('h4', cls=Host, ip='10.0.2.3', defaultRoute='via 10.0.2.1')
 
     info( '*** Add links\n')
     net.addLink(h1, s1)
     net.addLink(h2, s1)
     net.addLink(h3, s2)
     net.addLink(h4, s2)
+    # net.addLink(s2, r5,intfName2='r5-eth2',params2={'ip':'10.0.2.0/24'})
+    # net.addLink(s1, r3,intfName2='r3-eth2',params2={'ip':'10.0.1.0/24'})
     net.addLink(s2, r5)
     net.addLink(s1, r3)
-    net.addLink(r3, r4)
-    net.addLink(r4, r5)
-
+    net.addLink(r3, r4,intfName1='r3-eth1',params1={'ip':'192.168.1.1/30'}, intfName2='r4-eth1', params2={'ip':'192.168.1.2/30'})
+    net.addLink(r4, r5,intfName1='r4-eth0',params1={'ip':'192.168.2.2/30'}, intfName2='r5-eth1', params2={'ip':'192.168.2.1/30'})
     info( '*** Starting network\n')
     net.build()
+    # ___________________________________________________So far allows 27/42 connections 
+    net['r4'].cmd('ip route add 10.0.1.0/24 via 192.168.1.1 dev r4-eth1')
+    net['r4'].cmd('ip route add 10.0.2.0/24 via 192.168.2.1 dev r4-eth0')
+    
+    net['r3'].cmd('ip route add 10.0.2.0/24 via 192.168.1.2 dev r3-eth1')
+    
+    net['r5'].cmd('ip route add 192.168.1.0/30 via 192.168.2.2 dev r5-eth1')
+    net['r5'].cmd('ip route add 10.0.1.0/24 via 192.168.2.2 dev r5-eth1')
+    
+    # ___________________________________________________
     info( '*** Starting controllers\n')
     for controller in net.controllers:
         controller.start()
