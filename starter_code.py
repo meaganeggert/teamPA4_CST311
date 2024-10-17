222914795690759b85f6923a8cfb88fd9ac76669
HoppensTech <bhoppens@csumb.edu>
2024-10-17 12:16:34 -0700

    Imported makeTerm from mininet.term. Can now start xterm during startup and close on exit.

--- starter_code.py
+++ starter_code.py
@@ -8,6 +8,7 @@ from mininet.node import IVSSwitch
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from mininet.link import TCLink, Intf
+from mininet.term import makeTerm   # import makeTerm to allow xterm to run the chat server file using h4 on networks startup
 from subprocess import call
 
 def myNetwork():
@@ -23,19 +24,23 @@ def myNetwork():
                       port=6633)
 
     info( '*** Add switches\n')
-    s2 = net.addSwitch('s2', cls=OVSKernelSwitch)   #added switches before routers
+    # moved switches before routers
+    # mininet needs to create switches before the legacy routers
+    s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
-    r5 = net.addHost('r5', cls=Node, ip='10.0.2.0/24')
+    r5 = net.addHost('r5', cls=Node, ip='10.0.2.0/24')  # changed IP to have a subnet of 10.0.2.0/24 r5-eth0
     r5.cmd('sysctl -w net.ipv4.ip_forward=1')
-    r4 = net.addHost('r4', cls=Node, ip='192.168.1.1')
+    r4 = net.addHost('r4', cls=Node, ip='192.168.1.1')  # changed IP to have an IP of 192.168.1.1 r4-eth0
     r4.cmd('sysctl -w net.ipv4.ip_forward=1')
-    r3 = net.addHost('r3', cls=Node, ip='10.0.1.0/24')
+    r3 = net.addHost('r3', cls=Node, ip='10.0.1.0/24')  # changed IP to have a subnet of 10.0.1.0/24 r3-eth0
     r3.cmd('sysctl -w net.ipv4.ip_forward=1')
 
     info( '*** Add hosts\n')
-    h1 = net.addHost('h1', cls=Host, ip='10.0.1.1', defaultRoute='via 10.0.1.0')
+    # h1 and h2 has submnet 10.0.1.0/24 to match r3. (h1 = 10.0.1.1)(h2 = 10.0.1.2)
+    h1 = net.addHost('h1', cls=Host, ip='10.0.1.1', defaultRoute='via 10.0.1.0')  # added default route for h1 and h2 to forward to r3
     h2 = net.addHost('h2', cls=Host, ip='10.0.1.2', defaultRoute='via 10.0.1.0')
-    h3 = net.addHost('h3', cls=Host, ip='10.0.2.1', defaultRoute='via 10.0.2.0')
+    # h3 and h3 has submnet 10.0.2.0/24 to match r5. (h1 = 10.0.2.1)(h2 = 10.0.2.2)
+    h3 = net.addHost('h3', cls=Host, ip='10.0.2.1', defaultRoute='via 10.0.2.0')  # added default route for h3 and h4 to forward to r5
     h4 = net.addHost('h4', cls=Host, ip='10.0.2.2', defaultRoute='via 10.0.2.0')
 
     info( '*** Add links\n')
@@ -45,12 +50,14 @@ def myNetwork():
     net.addLink(h4, s2)
     net.addLink(s2, r5)
     net.addLink(s1, r3)
+    # added interfaces for r3-r4 using 192.168.1.0/30
     net.addLink(r3, r4,intfName1='r3-eth1',params1={'ip':'192.168.1.2/30'}, intfName2='r4-eth0', params2={'ip':'192.168.1.1/30'})
+    # added interfaces for r4-r5 using 192.168.2.0/30
     net.addLink(r4, r5,intfName1='r4-eth1',params1={'ip':'192.168.2.1/30'}, intfName2='r5-eth1', params2={'ip':'192.168.2.2/30'})
 
     info( '*** Starting network\n')
     net.build()
-    # ___________________________________________________hopefully correct way to route everything
+    # ___________________________________________________
     # allow hosts to send packets to its outside networks
     net['h1'].cmd('ip route add 10.0.2.0/24 via 10.0.1.0')
     net['h2'].cmd('ip route add 10.0.2.0/24 via 10.0.1.0')
@@ -79,9 +86,12 @@ def myNetwork():
     net.get('s1').start([c0])
 
     info( '*** Post configure switches and hosts\n')
-
+    # *** chat_server.py file needs to be renamed ***
+    # start xterm and run chat_server.py file
+    makeTerm(h4, title='Node', term='xterm', display=None, cmd='python3 chat_server.py; bash')
     CLI(net)
     net.stop()
+    net.stopXterms()    # close xterm on exit
 
 if __name__ == '__main__':
     setLogLevel( 'info' )
